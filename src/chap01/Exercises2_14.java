4484479874be2ee91cd73f312c21cecb375a06fe
akor11lyt <akor1@naver.com>
2021-12-28 22:43:43 +0900

    test commit

---
+++ src/chap01/Exercises2_14.java
@@ -0,0 +1,12 @@
+package chap01;
+
+public class Exercises2_14 {
+    public static void main(String[] args) {
+        System.out.printf("%d", 1);
+        System.out.print(" 2");
+        System.out.print(" ");
+        System.out.print("3");
+        System.out.print(" ");
+        System.out.println("4");
+    }
+}
